a9b607c142ff9a50fd95a35b3d5186eb3726f6de
NTDzVEKNY <141595521+NTDzVEKNY@users.noreply.github.com>
2024-10-31 16:14:01 +0700

    feat
    student management

--- Modules/UserManagement/Libraries/UserLib.php
+++ Modules/UserManagement/Libraries/UserLib.php
@@ -3,8 +3,37 @@
 namespace User\Libraries;
 
 use Auth\Models\AccountModel;
+use ReflectionException;
 
 class UserLib extends AccountModel
 {
+    public function getAllStudent(): array
+    {
+        return $this->where('flag', 2)->findAll();
+    }
 
+    public function getUserById($userId): object
+    {
+        return $this->where('id', $userId)->first();
+    }
+
+    public function deleteUser($userId): bool
+    {
+        return $this->delete(['id' => $userId]);
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @throws ReflectionException
+     */
+    public function createUser($data): bool
+    {
+        return $this->insert($data);
+    }
+
+    public function updateUser($id, $data): bool
+    {
+        return $this->update($id, $data);
+    }
 }
